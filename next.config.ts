ebbb169a7831a80ba2c8cd55ae41592f793da279
harrir11 <harrir11@tcnj.edu>
2025-08-28 12:29:55 -0400

    Add GitHub Pages deploy scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/egg_map",
+  assetPrefix: "/egg_map/",
+  images: { unoptimized: true },
 };
-
+module.exports = nextConfig;
 export default nextConfig;
